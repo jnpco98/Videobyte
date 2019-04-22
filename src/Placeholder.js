eeb55351fde595c438788d9fdaa7830b725c5997
JOR <jnpco.98@gmail.com>
2019-04-22 21:21:02 +1000

    Added Some placeholder data.

---
+++ src/Placeholder.js
@@ -0,0 +1,23 @@
+const Placeholder = {
+    accepted: [
+        {
+            name: "2_-zEN2AaneDn62xArnA.mp4",
+            path: "C:\Users\jnpco\Desktop\2_-zEN2AaneDn62xArnA.mp4",
+            preview: "blob:http://localhost:4680/55ecf102-d7f5-45dd-9135-79eaf044497f",
+            size: 104736102,
+            type: "video/mp4",
+        }
+
+    ],
+    rejected: [
+        {
+            name: "1_-zqAENneDn62xAKmrPTqnA.png",
+            path: "C:\Users\jnpco\Desktop\1_-zqAENneDn62xAKmrPTqnA.png",
+            preview: "blob:http://localhost:4680/e82c8125-b0ed-4622-96bd-f9d8b5387204",
+            size: 85072,
+            type: "image/png",
+        }
+    ]
+}
+
+export default Placeholder;
---
+++ src/VideoFormat.js
@@ -0,0 +1,16 @@
+const VideoFormat = {
+    '.avi': {},
+    '.flv': {},
+    '.mkv': {},
+    '.mov': {},
+    '.mpeg': {},
+    '.mpg': {},
+    '.mp4': {},
+    '.ogv': {},
+    '.vob': {},
+    '.webm': {},
+    '.wmv': {},
+    '.3gp': {},
+}
+
+export default VideoFormat;

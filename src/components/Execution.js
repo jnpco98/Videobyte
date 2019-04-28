7409f729f1f6033867016a78b46bbe4de70b2f5f
JOR <jnpco.98@gmail.com>
2019-04-28 20:10:44 +1000

    Added Execution components.

--- src/components/Execution.js
+++ src/components/Execution.js
@@ -11,26 +11,72 @@ const Wrapper = styled.div`
   padding: 0 1rem;
 `;
 
-const Label = styled.label`
+const SaveDirectory = styled.div``;
+
+const SaveToDirectoryCheck = styled.input.attrs({ type: 'checkbox' })`
+`;
+
+const SaveToDirectoryLabel = styled.label`
+`;
+
+const PathLabel = styled.label`
   margin-right: 0.8rem;
 `;
 
-const OutputFolder = styled.input`
+const PathName = styled.input`
   margin-right: 0.8rem;
   width: 50%;
 `;
 
-const Convert = styled.a`
+const Operation = styled.div`
+  display:flex;
+  align-items:center;
+  padding: 0 1rem;
 `;
 
-const Execution = ({ setSaveLocation, convert }) => {
-  return (
-    <Wrapper>
-      <Label> Folder: </Label>
-      <OutputFolder />
-      <Convert onClick={() => convert()}>Convert</Convert>
-    </Wrapper>
-  );
+const Cancel = styled.a`
+  background: green;
+`;
+
+const Convert = styled.a`
+  background: green;
+`;
+
+class Execution extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      saveToCurrentDirectory: true,
+    };
+  }
+
+  setSaveToCurrentDirectory = (enabled) => {
+    this.setState({ saveToCurrentDirectory: enabled });
+  }
+
+  render() {
+    const { setSaveLocation, convert } = this.props;
+    return (
+      <Wrapper>
+        <SaveDirectory>
+          <div>
+            <PathLabel> Folder: </PathLabel>
+            <PathName disabled={this.state.saveToCurrentDirectory} />
+          </div>
+          <div>
+            <SaveToDirectoryCheck checked={this.state.saveToCurrentDirectory} onChange={() => this.setSaveToCurrentDirectory(!this.state.saveToCurrentDirectory)} />
+            <SaveToDirectoryLabel> Save to current directory </SaveToDirectoryLabel>
+          </div>
+        </SaveDirectory>
+
+        <Operation>
+          <Cancel>Cancel</Cancel>
+          <Convert onClick={() => convert()}>Convert</Convert>
+        </Operation>
+      </Wrapper>
+    );
+  }
+
 };
 
 export default Execution;
